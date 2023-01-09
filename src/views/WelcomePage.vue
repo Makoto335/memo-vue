403465ff540f8e3c47cd14bc3db79fa6d14d7fa7
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-10 08:40:10 +0900

    Add basic style

--- src/views/WelcomePage.vue
+++ src/views/WelcomePage.vue
@@ -10,7 +10,7 @@
         <div v-if="showLoginForm">
           <LoginForm @redirectToMemoRoom="redirectToMemoRoom"/>
           <div class="WelcomePage_Text">
-            <p class="change-form">
+            <p class="WelcomePage_ChangeForm">
               <span @click="showLoginForm = false"
                 >アカウント登録はこちらをクリック</span
               >
@@ -20,7 +20,7 @@
         <div v-if="!showLoginForm">
           <SignupForm @redirectToMemoRoom="redirectToMemoRoom"/>
           <div class="WelcomePage_Text">
-            <p class="change-form">
+            <p class="WelcomePage_ChangeForm">
               <span @click="showLoginForm = true"
                 >ログインはこちらをクリック</span
               >
@@ -53,5 +53,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.WelcomePage {
+  span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+  // padding-top: 80px;
+  &_Wrapper {
+    display: flex;
+    width: 1280px;
+    margin: 60px auto;
+    // background: white;
+    // box-shadow: 4px 4px 4px 4px rgba(0,0,0,0.6);
+  }
+  &_AppDescription {
+    width: 50%;
+    // background: #51b392;
+    // background-image: linear-gradient(to right, #c6ffdd, #fbd786, #f7797d);
+  }
+  &_Form {
+    margin: 0 auto;
+    border-radius: 3px;
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.6);
+    // box-shadow: 2px 4px 6px rgba(28, 6, 49, 0.1);
+    background: white;
+    padding: 10px;
+  }
+  &_Text {
+    text-align: center;
+  }
+  &_ChangeForm {
+  font-size: 16px;
+  margin: 10px;
+}
+}
 </style>
+
