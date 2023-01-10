3a2f84bb50c21f4b7f66c4cff44567bea6322337
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-10 15:44:28 +0900

    Add getMessages method to show user's memos

--- src/views/MemoRoom.vue
+++ src/views/MemoRoom.vue
@@ -1,11 +1,42 @@
 <template>
-<div><Navbar /></div>
+  <div><Navbar /></div>
+  {{ memos }}
 </template>
 
 <script>
 import Navbar from "../components/NavBar";
+import axios from "axios";
 
-export default {components: { Navbar},};
+export default {
+  components: { Navbar },
+  data() {
+    return {
+      memos: [],
+    };
+  },
+  methods: {
+    async getMessages() {
+      try {
+        const res = await axios.get("http://localhost:3000/user", {
+          headers: {
+            uid: window.localStorage.getItem("uid"),
+            "access-token": window.localStorage.getItem("access-token"),
+            client: window.localStorage.getItem("client"),
+          },
+        });
+        if (!res) {
+          new Error("メッセージ一覧を取得できませんでした");
+        }
+        this.memos = res.data;
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+    mounted() {
+    this.getMessages();
+  },
+};
 </script>
 
 <style scoped lang="scss"></style>
