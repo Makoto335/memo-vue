57b6a72e09dbba97f4ea523eb1abb6ebf68715ec
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-10 17:17:35 +0900

    Add basic form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,9 +1,84 @@
 <template>
-<div>ログインフォーム</div>
+  <div class="LoginForm">
+    <h2>ログイン</h2>
+    <form @submit.prevent="login">
+      <input
+        type="email"
+        required
+        placeholder="メールアドレス"
+        v-model="email"
+      />
+      <input
+        type="password"
+        required
+        placeholder="パスワード"
+        v-model="password"
+      />
+      <div class="error">{{ error }}</div>
+      <div class="LoginForm_BtnWrapper"><button>ログインする</button></div>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+import setItem from "../plugins/auth/setItem";
+
+export default {
+  emits: ["redirectToMemoRoom"],
+  data() {
+    return {
+      email: "",
+      password: "",
+      error: null,
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        this.error = null;
+
+        const res = await axios.post("http://localhost:3000/auth/sign_in", {
+          email: this.email,
+          password: this.password,
+        });
+
+        if (!res) {
+          throw new Error("メールアドレスかパスワードが違います");
+        }
+        if (!this.error) {
+          setItem(res.headers, res.data.data.name);
+          this.$emit("redirectToMemoRoom");
+        }
+
+        console.log({ res });
+        return res;
+      } catch (error) {
+        console.log({ error });
+        this.error = "メールアドレスかパスワードが違います";
+      }
+    },
+  },
+};
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.LoginForm {
+  h2 {
+    text-align: center;
+  }
+  input {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px auto;
+    border-radius: 4px;
+    border: 1px solid #eee;
+    outline: none;
+    box-sizing: border-box;
+  }
+  &_BtnWrapper {
+    text-align: center;
+    margin: 20px auto;
+  }
+}
+</style>
