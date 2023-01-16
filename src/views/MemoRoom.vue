dfb48d2dca98deb50a2ca920eb5ca076c75876fd
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-16 17:55:32 +0900

    Add style and pagination

--- src/views/MemoRoom.vue
+++ src/views/MemoRoom.vue
@@ -1,19 +1,52 @@
 <template>
-  <div><Navbar /><MemoForm @getMessages="getMessages" /></div>
-
-  {{ memos }}
+  <div class="MemoRoom">
+    <Navbar />
+    <paginate
+      :page-count="getPaginateCount"
+      :page-range="3"
+      :margin-pages="1"
+      :click-handler="clickCallback"
+      :prev-text="'＜'"
+      :next-text="'＞'"
+      :container-class="'pagination'"
+      :page-class="'page-item'"
+    >
+      >
+    </paginate>
+    <div class="MemoRoom_Grid">
+       <MemoForm @getMessages="getMessages" />
+      <div
+        v-for="(memo, index) in getItems"
+        :key="index"
+        class="MemoRoom_Memos"
+      >
+        <div class="MemoRoom_BtnWrapper">
+          <button>edit</button>
+          <button>delete</button>
+        </div>
+        <h3>{{ memo.title }}</h3>
+        <p>{{ memo.content }}</p>
+        <div class="MemoRoom_CreateAt">
+          {{ memo.created_at }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from "../components/NavBar";
 import MemoForm from "../components/MemoForm";
 import axios from "axios";
+import Paginate from "vuejs-paginate-next";
 
 export default {
-  components: { Navbar, MemoForm },
+  components: { Navbar, MemoForm, Paginate },
   data() {
     return {
       memos: [],
+      currentPage: 1,
+      perPage: 11,
     };
   },
   methods: {
@@ -34,6 +67,23 @@ export default {
         console.log(err);
       }
     },
+    // reverseMemos() {
+    //   return this.memos.slice().reverse();
+    // },
+    clickCallback(pageNum) {
+      this.currentPage = Number(pageNum);
+    },
+  },
+  computed: {
+    getItems: function () {
+      let reversedMemos = this.memos.slice().reverse()
+      let start = (this.currentPage - 1) * this.perPage;
+      let end = this.currentPage * this.perPage;
+      return reversedMemos.slice(start, end);
+    },
+    getPaginateCount: function () {
+      return Math.ceil(this.memos.length / this.perPage);
+    },
   },
   mounted() {
     this.getMessages();
@@ -41,4 +91,64 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.MemoRoom {
+  .pagination {
+    text-align: center;
+    margin: 10 0 0 0;
+    padding: 0;
+    list-style-type: none;
+    // color: white;
+  }
+  &_Grid {
+    display: grid;
+    gap: 5px;
+    grid-template-columns: repeat(4, minmax(300px, 1fr));
+  }
+  &_Memos {
+    position: relative;
+    background: white;
+    padding: 1.5rem;
+    margin: 0.5rem;
+    width: 360px;
+    height: 200px;
+    h3 {
+      width: 180px;
+      height: 58px;
+      overflow: hidden;
+      margin-top: unset;
+      margin-bottom: 1rem;
+      font-weight: bold;
+      font-size: 1rem;
+    }
+    p {
+      width: 360px;
+      height: 100px;
+      overflow: hidden;
+      display: block;
+      margin: unset;
+      font-size: 1rem;
+      line-height: 1.25rem;
+    }
+  }
+  &_BtnWrapper {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    button {
+      width: 80px;
+      &:nth-child(2) {
+        margin-left: 20px;
+        background: rgb(239 68 68);
+      }
+    }
+  }
+  &_CreateAt {
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+    position: absolute;
+    bottom: 1rem;
+    right: 1.5rem;
+  }
+}
+</style>
