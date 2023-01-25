0eb40b96856f7317e83c3be606b70d45cc14c263
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-01-25 15:38:36 +0900

    Create EditForm

---
+++ src/components/modules/EditForm.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="EditForm">
+    <div class="EditForm_Overlay"></div>
+    <div class="EditForm_Wrapper">
+      <form @submit.prevent="handleCreate">
+        <label>
+          <span>Title</span>
+          <input class="EditForm_Title" type="text" v-model="title" />
+        </label>
+        <label>
+          <span>Content</span>
+          <textarea v-model="content"></textarea>
+        </label>
+        <input type="submit" value="保存する" />
+        <div class="error">{{ error }}</div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  props: ["idBeingEdited"],
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.EditForm {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 6;
+
+  &_Overlay {
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.8);
+    opacity: 0.2;
+  }
+  &_Wrapper {
+    position: absolute;
+    top: 48%;
+    left: 50%;
+    display: flex;
+    flex-direction: column;
+    transform: translate(-50%, -50%);
+    width: 500px;
+    height: 300px;
+    padding: 32px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border-radius: 4px;
+    border: 1px solid #333;
+  }
+  form {
+    margin: auto;
+    width: 300px;
+    label {
+      display: block;
+    }
+    textarea {
+      display: block;
+      padding: 0.5rem 1rem 0.5rem 1rem;
+      width: 100%;
+      height: 80px;
+    }
+  }
+  &_Title {
+    display: block;
+    width: 100%;
+    padding: 0.5rem 1rem 0.5rem 1rem;
+  }
+}
+</style>
