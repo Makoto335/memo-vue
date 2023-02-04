bc980205a457313611dee27b0c49f937ea2b9f59
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-02-04 17:52:26 +0900

    Add avatar image feature

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,26 +1,35 @@
 <template>
-  <nav>
-    <div>
-      <p>
-        <span class="name">{{ name }}</span
-        >さん
-      </p>
-      <div class="error">{{ error }}</div>
-    </div>
-    <button @click="logout">ログアウト</button>
-  </nav>
+  <div class="NavBar">
+    <nav>
+      <img class="NavBar_Avatar" :src="avatarInNav" />
+      <div>
+        <p>
+          <span class="NavBar_Name">{{ name }}</span
+          >さん
+        </p>
+        <div class="NavBar_Error">{{ error }}</div>
+      </div>
+      <button @click="logout">ログアウト</button>
+    </nav>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
 import removeItem from "../plugins/auth/removeItem";
 export default {
+  props: ["avatar"],
   data() {
     return {
       name: window.localStorage.getItem("name"),
       email: window.localStorage.getItem("uid"),
       error: null,
     };
+  },
+  computed: {
+    avatarInNav() {
+      return this.avatar;
+    },
   },
   methods: {
     async logout() {
@@ -55,20 +64,24 @@ export default {
 </script>
 
 <style scoped lang="scss">
-nav {
-  padding: 10px;
-  border-bottom: 1px solid #eee;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-nav p {
-  margin: 2px auto;
-  font-size: 16px;
-  color: #444;
-}
-nav p.email {
-  font-size: 14px;
-  color: #999;
+.NavBar {
+  nav {
+    padding: 10px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  p {
+    margin: 2px auto;
+    font-size: 16px;
+    color: #444;
+  }
+  &_Avatar {
+    display: block;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+  }
 }
 </style>
