360c86ca25fbf8aaffd107b45dcb182c317c5c53
MacMakoto <102998595+Makoto335@users.noreply.github.com>
2023-04-10 22:43:56 +0900

    Add devserver configration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+     allowedHosts: 'all',
+},
+configureWebpack: {
+  devtool: 'cheap-module-source-map'
+},
   transpileDependencies: true
 })
